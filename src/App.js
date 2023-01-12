845e6394ac39f47db33fd7d324cdf4acb0837dc7
d4niell <67240995+d4niell@users.noreply.github.com>
2023-01-12 09:05:56 +0000

    "data is undefined" will be fixed soon

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import Header from "./Header"
-import React from 'react';
+import React, { useEffect, useState } from "react";
+
 import Webtext from './Webtext'
 import Footer from './components/footer/footer.js'
-function SearchFunc() {
-  fetch("http://gis.vantaa.fi/rest/tyopaikat/v1")
-  .then(response => response.json())
-  .then((jsonData) => {
-    // jsonData is parsed json object received from url
-    console.log(jsonData)
-  })
-  .catch((error) => {
-    // handle your errors here
-    console.error(error)
-  })
-}
+
+
+
 
 function App() { 
-  SearchFunc() 
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch(`http://gis.vantaa.fi/rest/tyopaikat/v1`)
+      .then((response) => response.json())
+      .then((actualData) => {
+        console.log(actualData);
+        setData(actualData.products);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+useEffect(() => {
+fetchData();
+}, []);
+
   return (
     <div className="container">
      <Header />
      <Webtext />
-     <Footer/>
+<div className="data-testing-place">
+<p>below this line will begin my testing of data usage</p>
+<tbody>
+  <tr>
+  <th>Ammattiala</th>
+  </tr>
+  {data.map((ala, lisaa) => (
+    <tr key={ala}>
+      <td>ala.ammattiala</td>
+      <td>lisaa.linkki</td>
+    </tr>
+  ))}
+</tbody>
+</div>
     </div>
   );
 }
