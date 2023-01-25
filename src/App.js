cf5f72dfecaaf0634601461250b3433a41f85876
d4niell <67240995+d4niell@users.noreply.github.com>
2023-01-25 14:22:15 +0000

    beta data fetch

--- src/App.js
+++ src/App.js
@@ -8,34 +8,40 @@ import Footer from './components/footer/footer.js'
 
 
 function App() { 
-  const [data, setData] = useState([]);
-
-  const fetchData = () => {
-    fetch(`http://gis.vantaa.fi/rest/tyopaikat/v1`)
-      .then((response) => response.json())
-      .then((actualData) => {
-        console.log(actualData);
-        setData(actualData.products);
-        console.log(data);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+ 
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts?limit=1`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
       })
+      .then((actualData) => console.log(actualData))
       .catch((err) => {
         console.log(err.message);
       });
-  };
-
-useEffect(() => {
-fetchData();
-}, []);
+  }, []);
 
   return (
     <div className="container">
      <Header />
      <Webtext />
-     <ul>
+     <h1>testi nurkkaus</h1>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <ul>
         {data &&
-          data.map(({ id, ammattiala }) => (
+          data.map(({ id, title }) => (
             <li key={id}>
-              <h3>{ammattiala}</h3>
+              <h3>{title}</h3>
             </li>
           ))}
       </ul>
